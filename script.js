5b3b236b7c6821be8db6bb2149917c6fe5cd3d1f
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-27 11:05:32 +0100

    variables, arrays, objects, For, While, Do iterations

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// CONSOLE LOGGING
 console.log("Hello world!");
 const text = "here's a message";
 console.log(text);
@@ -26,6 +27,43 @@ let colouredOutput = "This is a colourful output";
 console.log("%c" + colouredOutput, "color: orange; font-family: fantasy; background-color: black; font-style: bold; padding: 10px;");
 
 
+
+// DATA TYPES 
+let isMutable = 10;
+const isImmutable = 12;
+var isInsecure = "don't use me";
+
+// 2 TYPES of types
+// primatives and objects
+
+let aBoolean = true 
+let aNumber = 4
+let aString = "some text"
+let aBigInt = 9999999999999999n;
+let aSymbol = Symbol("description in here");
+let aNull = null;
+let anUndefined;
+// objects
+let myObject = {
+    key : "value",
+    anotherKey : "this is another value"
+};
+
+console.log(myObject);
+console.log(myObject.anotherKey)
+
+// STRING CONCACTENATION
+let aString1 = "13 * 12 = ";
+let aResult = 13*12;
+console.log(aString1 + aResult);
+
+// STRING INTERPOLATION
+// using literals containing placeholders
+// called template literals
+let aString2 = `13 * 12 = ${13*12}`;
+console.log(aString2);
+
+
 let a;
 let b = "12345";
 let c = 54321;
@@ -40,17 +78,35 @@ console.log(e + " " + typeof(e));
 let totalMoney = 4000
 let moneyPaidSoFar = 2348
 let moneyLeft = totalMoney-moneyPaidSoFar
-console.log("The total bill is £",totalMoney, " the amount remaining is £", moneyLeft)
+console.log(`The total bill is £${totalMoney}, the amount remaining is £${moneyLeft}`)
+
+
+// ARRAYS
+let mySword = {
+    "type": "greatsword",
+    "damage": 50,
+};
+
+let myShield = {
+    "type": "greatshield",
+    "defense": 120
+};
+
+let myInventory = [mySword, myShield, "health potion"];
+console.log(myInventory);
 
 
 
+// ITERATION
+// FOR
 for (let i = 0; i < 10; i++){
     console.log(i)
-}
+};
 
-let puppies = 0
-let notEnoughPuppies = true
+let puppies = 0;
+let notEnoughPuppies = true;
 
+// WHILE: checks condition first
 while (notEnoughPuppies) {
     console.log("Another puppy");
     puppies++;
@@ -60,4 +116,15 @@ while (notEnoughPuppies) {
         let enough = "Enough puppies!";
         console.log(enough);
     }
-}
+};
+
+// DO WHILE: executes onces first before checking condition
+let x = 0;
+let canIRun = false;
+do {
+    x++;
+    console.log(x);
+} while (canIRun);;
+
+
+
