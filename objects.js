50e7efd5478f93e5eae2613f96a33ebd7a0f02c6
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-27 14:37:07 +0100

    destructuring

--- objects.js
+++ objects.js
@@ -1,5 +1,43 @@
 'use strict';
 
+let myInventory = []
+myInventory.push("sword");
+myInventory.push("potion");
+myInventory.push("shield");
+console.log(myInventory);
+console.log(myInventory.sort());
+console.log(myInventory.reverse());
+console.log(myInventory.join());
+console.log(myInventory.sort().join());
+
+myInventory.unshift("helmet");
+console.log(myInventory);
+myInventory.shift("helmet");
+console.log(myInventory);
+console.log(myInventory.length);
+
+for (let item of myInventory) {
+    console.log(item);
+}
+
+//DESTRUCTURING AN ARRAY
+const myKatanas = ["Uchigatana", "Masamune", "Muresame", "Nagakiri", "Moon's Veil"]
+const [a,b,c,d,e] = myKatanas;
+console.log(a);
+console.log(c);
+//SPREAD OPERATOR ...
+const [w,x,...rest] = myKatanas;
+console.log(x);
+console.log(rest);
+// SKIPPING ELEMENTS - use , to separate
+const[g,,i,j,k] = myKatanas
+console.log(g);
+console.log(i);
+
+
+
+
+
 //1.
 // let darthVader = new Object();
 // darthVader["allegiance"] = "empire";
