4454a8c12d7bc4443cc5003971c4ffb3956e2c5e
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-27 10:19:33 +0100

    updating

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 console.log("Hello world!");
 const text = "here's a message";
@@ -9,20 +9,49 @@ console.warn(text);
 console.error(text);
 console.log("%c" + text, "color: black; background-color: white; font-size: 20px; padding: 5px;");
 
+let firstName = "Jonathan";
+let lastName = "Stewart";
+let myLocation = "Glasgow";
+let starSign = "Pisces";
+
+console.log("My name is " + firstName + " " + lastName + ". I am from " + myLocation + " and I am a " + starSign);
+
+let carName = "Nissan";
+let carMake = "GT-R";
+
+console.log("My favourite car make is" + carName + " and the model is " + carMake);
+
+let colouredOutput = "This is a colourful output";
+
+console.log("%c" + colouredOutput, "color: orange; font-family: fantasy; background-color: black; font-style: bold; padding: 10px;");
+
+
+let a;
+let b = "12345";
+let c = 54321;
+let d = true;
+let e = {code: "Javascript"}
+console.log(a);
+console.log(b)
+
+
+
+
+
 for (let i = 0; i < 10; i++){
     console.log(i)
 }
 
 let puppies = 0
-let not_enough_puppies = true
+let notEnoughPuppies = true
 
-while (not_enough_puppies) {
+while (notEnoughPuppies) {
     console.log("Another puppy");
     puppies++;
 
     if (puppies>20) {
-        not_enough_puppies = false
-        let enough = "Enough puppies!"
+        notEnoughPuppies = false;
+        let enough = "Enough puppies!";
         console.log(enough);
     }
 }
