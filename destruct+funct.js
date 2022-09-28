d8abb61d6c74d61a05e74c4358f1b6fbff83a7b4
JonathanStewart2 <jonathan.stewart2@hotmail.com>
2022-09-28 12:26:18 +0100

    function exercises

--- destruct+funct.js
+++ destruct+funct.js
@@ -27,3 +27,28 @@ console.log(subtract(10,5));
 console.log(subtract(100,67));
 console.log(subtract(10,50));
 console.log(subtract("hello","o"));
+
+//2. Create a function expression called welcome that take in name,
+// age,gender as a parameters. The outcome should be like so:
+// e.g. "My name is Felix Cited, i am 20 years old and of gender 
+// Male"
+
+const welcome = function(name,age,gender) {
+    console.log(`My name is ${name}, I am ${age} years old 
+    and of the gender ${gender}`);
+}
+
+welcome("Jimmy",45,"male");
+
+//3. 
+//Create an arrow function called powerUp that takes in two values 
+//n1 and n2. The arrow function will return the power of the two 
+//numbers.
+
+const powerUp = (n1, n2) => {
+    return Math.pow(n1, n2);
+}
+console.log(powerUp(2,3));
+
+let powerUp2 = (n3,n4) => Math.pow(n3,n4);
+console.log(powerUp2(4,2));
