fbfa12aadabb4d49fa2470c104ec49b89b63f3e1
JonathanStewart2 <jonathan.stewart2@hotmail.com>
2022-09-28 09:51:51 +0100

    update

--- js-recap.js
+++ js-recap.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// print to console
+
+// CONSOLE OUTPUT
 console.log("something");
 console.error("LOG ERROR HERE");
 console.warn("warning"); // warning, not an error
@@ -14,7 +15,8 @@ console.log("group output");
 console.log("group output");
 console.groupEnd();
 
-// Variables: 3 types
+
+// VARIABLES: 3 types
 // const - once assigned value, is immutable
 // let - block scoped, can is mutable
 // var - do not use, scope leaks
@@ -23,12 +25,26 @@ console.log("data type of ",a,"  = ",typeof a);
 a = 123
 console.log("data type of ",a,"  = ",typeof a);
 
-// Data Types
+
+// DATA TYPES
 // string, boolean, symbol, null, bigint, undefined, number, NaN
 console.log(typeof(true));
 
+
 // OPERATORS
-// +,-,/,*.>,<,>=,<=,==,===, !=
+// +,-,/,*.>,<,>=,<=,==,===, !=, !==, &&, ||,
+// && and, || or - logical
+// &, | - bitwise - better to use on Boolean datatypes
 // = - assigning operator
 // == - compares & checks equality of values
+console.log(1 == "1"); //true
 // == - checks equality of values and data types
+console.log(1 === "1"); //false
+
+
+// ITERATION
+// loops - repeat instructions for certain amount of time
+// FOR - when you know the number of iterations required
+// WHILE: continue loop until condition is met
+// DO WHILE: execute at least once and continue until condition is met
+
