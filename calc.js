95682e3d123daa1b6242fd2fa4a42ca9be5372bd
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-29 14:48:20 +0100

    updating

--- calc.js
+++ calc.js
@@ -42,84 +42,59 @@ const tangent = (deg) => {
 };
 
 
-// testing
-console.log(addition(4,13));
-console.log(subtract(4,13));
-console.log(multiply(4,13));
-console.log(divide(4,13));
-console.log(power(4));
-console.log(remainder(100,9));
-console.log(discount(100,25));
-// testing SQE function
-console.log(squareRootEstimate(16));
-console.log(squareRootEstimate(25));
-console.log(squareRootEstimate(77));
-console.log(squareRootEstimate(100));
-// testing sine
-console.log(sine(0)); // 0
-console.log(sine(90)); // 1
-console.log(sine(180)); // 0
-console.log(sine(270)); // -1
-console.log(sine(360)); // 0
-// testing cos
-console.log(`Cos of 0 = ${cosine(0)}`); //  1
-console.log(`Cos of 90 = ${cosine(90)}`); // 0
-console.log(`Cos of 180 = ${cosine(180)}`); // -1
-console.log(`Cos of 270 = ${cosine(270)}`); // 0
-console.log(`Cos of 360 = ${cosine(360)}`); // 1
-// testing tan
-console.log(`Tan of 0 = ${tangent(0)}`); //  0
-console.log(`Tan of 90 = ${tangent(90)}`); // some big number
-console.log(`Tan of 180 = ${tangent(180)}`); // 0
-console.log(`Tan of 270 = ${tangent(270)}`); // some big number
-console.log(`Tan of 360 = ${tangent(360)}`); // 0
+// // testing -----------------
+// console.log(addition(4,13));
+// console.log(subtract(4,13));
+// console.log(multiply(4,13));
+// console.log(divide(4,13));
+// console.log(power(4));
+// console.log(remainder(100,9));
+// console.log(discount(100,25));
+// // testing SQE function
+// console.log(squareRootEstimate(16));
+// console.log(squareRootEstimate(25));
+// console.log(squareRootEstimate(77));
+// console.log(squareRootEstimate(100));
+// // testing sine
+// console.log(sine(0)); // 0
+// console.log(sine(90)); // 1
+// console.log(sine(180)); // 0
+// console.log(sine(270)); // -1
+// console.log(sine(360)); // 0
+// // testing cos
+// console.log(`Cos of 0 = ${cosine(0)}`); //  1
+// console.log(`Cos of 90 = ${cosine(90)}`); // 0
+// console.log(`Cos of 180 = ${cosine(180)}`); // -1
+// console.log(`Cos of 270 = ${cosine(270)}`); // 0
+// console.log(`Cos of 360 = ${cosine(360)}`); // 1
+// // testing tan
+// console.log(`Tan of 0 = ${tangent(0)}`); //  0
+// console.log(`Tan of 90 = ${tangent(90)}`); // some big number
+// console.log(`Tan of 180 = ${tangent(180)}`); // 0
+// console.log(`Tan of 270 = ${tangent(270)}`); // some big number
+// console.log(`Tan of 360 = ${tangent(360)}`); // 0
+// ------------------------------------------
 
+const createPageTitles = () => {
+    let newTitle = document.createElement("title");
+    newTitle.innerText = ("Online Calculator");
+    document.head.appendChild(newTitle);
 
-
-
-
-
-
-// EXPERIMENTING CALCULATOR v2
-
-// modulo function
-const modulo10 = (value) => {
-    console.log(value%10);
-}
-// power of function
-const powerIt = (value) => {
-    console.log(value * value);
-}
-// square root child function
-const squareRootIt = (value) => {
-    let count = 0;
-    let step = 1;
-    while (value > 0){
-        value -= step;
-        count++;
-        step += 2;
-        }
-    console.log(count);
-}
-//MAIN FUNCTION
-const setValue = (alterIt, num1) => {
-    alterIt(num1);
+    let topH1 = document.querySelector("h1");
+    topH1.innerText = "Online Calculator";
+    document.body.appendChild(topH1)
 }
 
-
-
-setValue(modulo10, 99);
-setValue(modulo10, 2);
-setValue(modulo10, 100);
-
-for (let i=0; i<10; i++) {
-    setValue(powerIt, i);
+const createTopRow = () => {
+    let divEle = document.querySelector("div");
+    const button9 = document.createElement("button");
+    button9.innerText = "9";
+    divEle.append(button9);
 }
 
+const main = () => {
+    createPageTitles();
+    createTopRow();
+}
 
-setValue(squareRootIt, 16);
-setValue(squareRootIt, 46);
-
-setValue(squareRootIt, 16);
-setValue(squareRootIt, 46);
-
+main();
--- dom-exercise.js
+++ dom-exercise.js
@@ -3,6 +3,16 @@
 //Write a function that creates a new h1 element, adds text to that 
 //element and then adds the h1 to the tree of the document on load 
 //of the HTML page.
+
+// let breakPt = "<\ br>";
+// document.body.append(breakPt);    - doesnt work
+
+
+// BUTTONS
+const colourChangeButton = document.createElement("button");
+colourChangeButton.innerText = "Title Colour Changer";
+document.body.append(colourChangeButton); 
+
 const titleButton = document.createElement("button");
 titleButton.innerText = "Add Title";
 document.body.append(titleButton);
@@ -11,6 +21,14 @@ const para1Button = document.createElement("button");
 para1Button.innerText = "Add Paragraph";
 document.body.append(para1Button);
 
+
+
+// const addChildButton = document.createElement("button");
+// addChildButton.innerText = "Add Child";
+// document.body.append(addChildButton);
+
+
+// MANIPULATION FUNCTIONS
 const createH1 = () => {
     let newH1 = document.createElement("h1");
     newH1.innerText = prompt("Write a heading");
@@ -19,11 +37,78 @@ const createH1 = () => {
 
 const addPara = () => {
     let newPara = document.createElement("p");
+    newPara.id = "paraID"
     newPara.innerText = prompt("Write your paragraph");
     document.body.appendChild(newPara);
 }
 
 
+const colourChange = () => {
+    let div1 = document.querySelector("div");
+    if (div1.style.color == "black"){
+        div1.style.color = "orange";
+    } else if (div1.style.color == "orange"){
+        div1.style.color = "green";
+    } else if (div1.style.color == "green"){
+        div1.style.color = "blue";
+    } else {
+        div1.style.color = "black";
+    }
+}
+
+// const removeChild = (element) => {
+//     element.removeChild();
+// }
+
+// const addChild = (element) => {
+//     let newText = document.createElement("p");
+//     newText.textContent = (prompt("Gimme some text"));
+//     element.appendChild(newText);
+// }
+
+
+// DIV FINDER
+let div1 = document.querySelector("div");
+div1.style.color = "orange";
+
+// CHILD CREATION
+let moreText = document.createElement("p");
+moreText.textContent = "This is a new paragraph";
+div1.appendChild(moreText);
+
+
+// BUTTON LISTENERS
 titleButton.addEventListener("click", createH1);
 para1Button.addEventListener("click", addPara);
+colourChangeButton.addEventListener("click", colourChange);
+// removeChildButton.addEventListener("click", removeChild(div1));
+// addChildButton.addEventListener("click", addChild(div1));
+
+div1.setAttribute("attr", 10);
+console.log(div1.getAttribute("attr"));
+div1.removeAttribute("attr");
+console.log(div1.getAttribute("attr"));
+
+
+const makePara = () => {
+    const newPara = document.createElement("p");
+    newPara.id = "paraID2";
+    newPara.innerText = "New paragraph";
+    document.body.appendChild(newPara)
+}
+
+makePara();
+
+const updatePara = () => {
+    let existingPara = document.getElementById("paraID2");
+    console.log(existingPara);
+    let newText = document.getElementById("paraText").value;
+    console.log(newText);
+    existingPara.innerText = newText;
+}
+
+const deletePara = () => {
+    let ePara = document.getElementById("paraID2");
+    ePara.remove()
+}
 
