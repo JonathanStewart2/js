19c351069f53bcbc98bb99b97d753fadf97808e5
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-29 12:04:46 +0100

    DOM manip

---
+++ dom-manipulation.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Document Object Model
+
+//                  DOCUMENT
+//                     |
+//                  ROOT ELEMENT <html>
+//                  /               \
+//              ELEMENT               ELEMENT
+//              <head>                 <body>
+//                                      /    \
+//                                    <h1>    <div>
+
+const title = document.createElement("h1"); // creates h1 tag
+title.innerText = "Let's manipulate the DOM"; // adds text inbetween h1 tags
+document.body.appendChild(title); // h1 added to body
+
+const div = document.getElementById("myDiv"); // finds specific element id=myDiv
+console.log(div);
+
+const newButton = document.createElement("button"); // creates a button element
+newButton.innerText = "PUSH ME"; // updates text on button
+div.prepend(newButton); //adds button to div we retrieved
+
+
+const greet = () => {
+    console.log("Hello World");
+}
+
+newButton.onclick = greet;
