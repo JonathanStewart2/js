44261ee207b8e515e477deb827e800f871bf9e5b
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-30 14:06:56 +0100

    requests exercises

--- Data Requests.js
+++ Data Requests.js
@@ -50,7 +50,7 @@
 //     }
 // }
 
-const url = "https://reqres.in/api/users";
+
 
 // fetch(url)
 // .then(function(response){
@@ -65,6 +65,12 @@ const url = "https://reqres.in/api/users";
 
 
 
+
+// EXERCISES ---------------
+
+
+const url = "https://reqres.in/api/users";
+
 axios.get(url)
 .then(response => appendData(response.data))
 .catch(err => console.error(err));
@@ -76,28 +82,121 @@ const appendData = (data) => {
     for(let i = 0; i< subData.length; i++){
         let person = document.createElement("div");
         let personID = document.createElement("p");
+        let personIDStrong = document.createElement("h1");
         //console.log(data[i]);
-        personID.innerText = "ID: " + subData[i].id;
+        personIDStrong.innerText = "ID: " + subData[i].id;
+        personID.appendChild(personIDStrong);
         console.log(personID.innerText);
         person.appendChild(personID);
 
-        let personEmail = document.createElement("p");
+        let personEmail = document.createElement("h4");
         personEmail.innerText = "Email: " + subData[i].email;
         person.appendChild(personEmail);
         
         let personName = document.createElement("p");
-        personName.innerText = "Name: " + subData[i].first_name + " " + subData[i].last_name;
+        let personNameUL = document.createElement("u");
+        personNameUL.innerText = "Name: " + subData[i].first_name + " " + subData[i].last_name;
+        personName.appendChild(personNameUL);
         person.appendChild(personName);
 
         let personImg = document.createElement("img");
         personImg.src = (`${subData[i].avatar}`);
         person.appendChild(personImg);
 
+        let breakLine = document.createElement("hr");
+        person.appendChild(breakLine);
         dataList.appendChild(person);
     }
 }
 
+// GET REQUEST
 
-// axios.get(url)
-// .then(response => console.log(response.data))
-// .catch(err => console.error(err));
+//1. GET request
+// fetch(url)
+// .then(function(response){
+//     return response.json();
+// })
+// .catch(function(err){
+//     console.error(err);
+// })
+// .then(function(data){
+//     console.log(data);
+// })
+
+//2. GET request for single user with ID: 2
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+.catch(function(err){
+    console.error(err);
+})
+.then(function(data){
+    let userDetails = data.data
+
+    for (let i=0; i < userDetails.length; i++){
+        if (userDetails[i].id == 2){
+            console.log(userDetails[i]);
+        }
+    }
+})
+
+//3 POST request for 'Create'
+// name with a value of "Morpheus"
+// job with a value of "Leader"
+
+fetch(url, {
+    method: "post",
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify(
+        {
+            "first_name": "Morpheus",
+            "job" : "Leader"
+        }
+    )
+}).then(response => response.json())
+.then(data => console.log(`Request succeeded: ${JSON.stringify(data)}`))
+.catch(error => console.log(`Request Failed: ${error}`));
+
+//4 POST request for 'Register - Successful'
+// email with a value of "eve.holt@reqres.inheus"
+// password with a value of "pistol"
+
+let url2 = "https://reqres.in/api/register";
+
+fetch(url, {
+    method: "post",
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify(
+        {
+            "email": "eve.holt@reqres.inheus",
+            "password": "pistol"
+        }
+    )
+}).then(response => response.json())
+.then(data => console.log(`Request succeeded ${JSON.stringify(data)}`))
+.catch(error => console.log(`Request Failed: ${error}`));
+
+//5 POST request for 'Login - Successful'
+// email with a value of "eve.holt@reqres.inheus"
+// password with a value of "cityslicka"
+let loginURL = "https://reqres.in/api/login";
+
+fetch(loginURL, {
+    method: "post",
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify(
+        {
+            "email": "eve.holt@reqres.inheus",
+            "password": "cityslicka"
+        }
+    )
+}).then(response => response.json())
+.then(data => console.log(`Request succeeded ${JSON.stringify(data)}`))
+.catch(error => console.log(`Request Failed: ${error}`));
