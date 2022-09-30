8151acf7f6d843d2f99c2ec99548efecd9c6bc9b
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-09-30 10:47:11 +0100

    data requests

---
+++ Data Requests.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// REQUEST       HTTP
+// Create        POST
+// Read          GET
+// Update        PUT/PATCH
+// Delete        DELETE
+
+// PUT replaces all values with new ones, PATCH replaces selected ones
+// GET is only method we don't pass any variables in to
+
+// STATUS CODE: 
+// 100-199 -> informational response
+// 200-299 -> successful response
+// 300-399 -> redirect       (301 = moved permanently)
+// 400-499 -> client errors  (404 - not found)
+// 500-599 -> server errors  (500 - internal server error)
